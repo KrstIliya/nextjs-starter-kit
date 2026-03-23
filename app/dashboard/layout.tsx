7ea2b8865e2592310201b38dd68a9361d1924c84
KrstIliya <krsiy@proton.me>
2026-03-23 13:53:59 +0100

    bug fix v0.4

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react";
-import DashboardShell from "./_components/navbar";
 
 export default async function DashboardLayout({
   children,
@@ -8,9 +7,7 @@ export default async function DashboardLayout({
 }) {
   return (
     <div className="flex flex-col h-screen w-full space-gradient-bg">
-      <DashboardShell>
-        {children}
-      </DashboardShell>
+      {children}
     </div>
   );
 }
